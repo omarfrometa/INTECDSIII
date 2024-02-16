5d294e9e91f3f1de68a86976e38a27ffe036abce
Omar Frometa <omar.frometa@fvtech.net>
2024-02-15 19:52:01 -0500

    BrithPlace Entity

---
+++ INTEC.Project2/Entities/BirthPlaceEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTEC.Project2.Entities
+{
+    public class BirthPlaceEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Enabled { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
